a97d9e189f10fa233ac1ea1348f53c63793c64ff
Sebastian Bejm <125sbejm@gmail.com>
2020-09-25 12:00:24 -0700

    added base controls

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+
+    private float bottomBound = -6.0f;
+    private float topBound = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Input for left paddle (main player)
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 paddlePos = new Vector3(0, 0.1f, 0);
+            transform.Translate(paddlePos);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Vector3 paddlePos = new Vector3(0, -0.1f, 0);
+            transform.Translate(paddlePos);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PaddleController2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController2 : MonoBehaviour
+{
+    public bool secondPlayer = true;
+
+    private float bottomBound = -6.0f;
+    private float topBound = 6.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Input for right paddle (2nd player)
+        if (secondPlayer)
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Vector3 paddlePos = new Vector3(0, 0.1f, 0);
+                transform.Translate(paddlePos);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Vector3 paddlePos = new Vector3(0, -0.1f, 0);
+                transform.Translate(paddlePos);
+            }
+        }
+    }
+}
