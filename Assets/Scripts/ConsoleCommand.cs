327ff66b07c3eb0c37897bee436706b94627f8d4
JasonZFHuang <jasonhuangzf@gmail.com>
2020-09-27 09:14:45 -0700

    console commands

---
+++ Assets/Scripts/ConsoleCommand.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+public class ConsoleCommand : MonoBehaviour
+{
+    public bool awake = false;
+    public GameObject console;
+    public InputField input;
+    public GameObject background;
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        console.SetActive(awake);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (awake == true)
+        {
+            //focus input
+            input.ActivateInputField();
+
+            //close console
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                awake = false;
+                console.SetActive(false);
+            }
+
+            // submit
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Submit(input.text);
+            }
+
+
+
+            
+
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            awake = true;
+            console.SetActive(true);
+        }
+
+    }
+
+    private void Submit(string text)
+    {
+
+        input.text = "";
+
+        if (text == "red")
+        {
+            background.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (text == "green")
+        {
+            background.GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (text == "blue")
+        {
+            background.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (text == "white")
+        {
+            background.GetComponent<Renderer>().material.color = Color.white;
+        }
+        if (text == "ballred")
+        {
+            ball.GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (text == "ballgreen")
+        {
+            ball.GetComponent<Renderer>().material.color = Color.green;
+        }
+        if (text == "ballblue")
+        {
+            ball.GetComponent<Renderer>().material.color = Color.blue;
+        }
+        if (text == "ballwhite")
+        {
+            ball.GetComponent<Renderer>().material.color = Color.white;
+        }
+
+    }
+
+
+}
--- README.md
+++ README.md
@@ -5,3 +5,41 @@ A simple game of Pong where players can control paddles to hit the ball. Made us
 * Left Player: W to go up, S to go down
 * Right Player: Up Arrow to go up, Down Arrow to go down
 * Start New Round: Space Bar
+* Press C while in game to open console
+* Press Esc to exit console
+
+**Console Commands**
+
+*All commands are case sensitive*
+
+> red
+
+Sets background to red.
+
+> blue
+
+Sets background to blue.
+
+> green
+
+Sets background to green.
+
+> white
+
+Sets background to white.
+
+> ballred
+
+Sets ball to red.
+
+> ballblue
+
+Sets ball to blue.
+
+> ballgreen
+
+Sets ball to green.
+
+> ballwhite
+
+Sets ball to white.
